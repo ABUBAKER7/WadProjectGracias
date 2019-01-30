087e10a6232e754c4cbbd16ffe1f66e0a17bb9a0
MaroofAkram <44575901+MaroofAkram@users.noreply.github.com>
2019-01-30 09:47:54 +0500

    updated forget passward by Mroofakram
    updated forget passward by Mroofakram

---
+++ check_email.php
@@ -0,0 +1,8 @@
+<?php
+include "connection.php";
+$e = $_REQUEST["e"];
+$sel_email = "select * from registration where email= '$e'";
+$run_email  = mysqli_query($con,$sel_email);
+$count = mysqli_num_rows($run_email);
+if($count>0)
+    echo "Already registered";
